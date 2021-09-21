6549a61660aa8baf1e14d323706ad8f4a5709784
AkashRamkar <akashrramkar2001@gmail.com>
2021-09-21 13:33:25 +0530

    added firebase dependency

---
+++ app/src/main/java/com/example/firebasesignindemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.firebasesignindemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//splash screen activity
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
